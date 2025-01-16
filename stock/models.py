6f1a5cf5b6c936b2633ad29d154bfc5964cfff39
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-01-16 14:55:02 -0300

    feat: create migrations, create view get/post products and configure route

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+class ProductModel(BaseModel):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from stock import models, serializers
+
+
+@api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+def product_list_create(request):
+    """
+    List and Create Products
+    """
+    if request.method == 'POST':
+        serializer = serializers.ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    products = models.ProductModel.objects.all()
+    serializer = serializers.ProductSerializer(products, many=True)
+    return Response(serializer.data)
