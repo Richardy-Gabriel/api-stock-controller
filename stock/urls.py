d28f82fea287e7b654a343054de917c882a600fb
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-01-16 16:20:25 -0300

    feat: update and delete product created

--- stock/urls.py
+++ stock/urls.py
@@ -3,4 +3,5 @@ from stock import views
 
 urlpatterns = [
     path('products/', views.product_list_create, name="product-list-create"),
+    path('products/<int:pk>/', views.product_detail, name="product-detail"),
 ]
--- stock/views.py
+++ stock/views.py
@@ -23,3 +23,27 @@ def product_list_create(request):
     products = models.ProductModel.objects.all()
     serializer = serializers.ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    try:
+        product = models.ProductModel.objects.get(pk=pk)
+    except models.ProductModel.DoesNotExist:
+        return Response({
+            "error": "The product is not registered"
+        }, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "PUT":
+        serializer = serializers.ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    serializer = serializers.ProductSerializer(product)
+    return Response(serializer.data)
